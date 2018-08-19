ccda981fd1597f050892b33e1762f0c2de67c328
Jingfei Qin <qinjingfei1@gmail.com>
2018-08-19 16:29:01 +0800

    完成歌手数据抓取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,3 +1,33 @@
 <template>
     <h1>this is singer component</h1>
 </template>
+<script>
+import {getSingerList} from '@/api/singer'
+import {ERR_OK} from '@/api/config'
+
+export default {
+    data() {
+        return {
+            singers: []
+        }
+    },
+    created() {
+        this._getSingerList()
+    },
+    methods: {
+        _getSingerList() {
+            getSingerList().then(res => {
+                if (res.code === ERR_OK) this.singers = res.data.list
+            })
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.singer
+    position: fixed
+    top: 88px
+    bottom: 100%
+    width: 100%
+</style>

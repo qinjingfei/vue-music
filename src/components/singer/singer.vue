81f5867e812d17f2f0136ad632de4c7ccf0c0852
Jingfei Qin <qinjingfei1@gmail.com>
2018-08-20 11:17:53 +0800

    完成滚动列表实现

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>this is singer component</h1>
+    <div class="singer">
+        <list-view :data="singers"></list-view>
+    </div>
 </template>
 <script>
 import {getSingerList} from '@/api/singer'
 import {ERR_OK} from '@/api/config'
+import Singer from 'common/js/singer.js'
+import ListView from '@/base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 
 export default {
     data() {
@@ -17,17 +24,62 @@ export default {
     methods: {
         _getSingerList() {
             getSingerList().then(res => {
-                if (res.code === ERR_OK) this.singers = res.data.list
+                if (res.code === ERR_OK) this.singers = this._normalizeSinger(res.data.list)
             })
+        },
+        _normalizeSinger(list) {
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
+                }
+            }
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LEN) {
+                    map.hot.items.push(new Singer({
+                        id: item.Fsinger_mid,
+                        name: item.Fsinger_name
+                    }))
+                }
+                const key = item.Findex
+                if (!map[key]) {
+                    map[key] = {
+                        title: key,
+                        items: []
+                    }
+                }
+                map[key].items.push(new Singer({
+                    id: item.Fsinger_mid,
+                    name: item.Fsinger_name
+                }))
+            })
+            // 得到有序列表
+            let hot = []
+            let ret = []
+            for (let key in map) {
+                let val = map[key]
+                if (val.title.match(/[a-zA-Z]/)) {
+                    ret.push(val)
+                } else if (val.title === HOT_NAME) {
+                    hot.push(val)
+                }
+            }
+            ret.sort((a, b) => {
+                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+            })
+            return hot.concat(ret)
         }
+    },
+    components: {
+        ListView
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-.singer
+  .singer
     position: fixed
     top: 88px
-    bottom: 100%
+    bottom: 0
     width: 100%
 </style>
